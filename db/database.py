d7d4fe6df708101ad5f5ca2389d1f0e042b774dd
Gundue <gunwoo1919@naver.com>
2024-01-18 11:43:07 +0900

    [FEAT] user model, schema 추가

--- db/database.py
+++ db/database.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+from db import *
+
 SQL_ALCHEMY_DATABASE_URL = (
     f"mysql+pymysql://{DB_ID}:{DB_PW}@"
     f"{DB_HOST}:{DB_PORT}/{DB}"
 )
 
 engine = create_engine(
-    SQL_ALCHEMY_DATABASE_URL,
-    encodings="utf-8"
+    SQL_ALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
