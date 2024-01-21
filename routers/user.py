c220c6f69cbb03cb5713d688c6a7f04748fa99bf
Gundue <gunwoo1919@naver.com>
2024-01-21 21:44:56 +0900

    [FEAT] logout method 구현

--- routers/user.py
+++ routers/user.py
@@ -1,13 +1,15 @@
 import bcrypt
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Header
 from sqlalchemy.orm import Session
 from starlette import status
+from datetime import datetime
 
+import models
 from crud.crud_user import read_user, create_user
+from crud.crud_token import create_token, read_token, update_token, read_token_chk, delete_token
 from core.auth.jwt import encode_jwt
 from db.database import engine
-import models
-from schemas import user_schema
+from schemas import user_schema, token_schema
 from routers import get_db
 
 prefix = "user"
@@ -30,7 +32,7 @@ async def user_create(user: user_schema.User,  db: Session = Depends(get_db)):
 
 
 @router.post("/login")
-async def lgoin(user: user_schema.UserLogin,  db: Session = Depends(get_db)):
+async def login(user: user_schema.UserLogin,  db: Session = Depends(get_db)):
     """
     `로그인`
     """
@@ -38,13 +40,29 @@ async def lgoin(user: user_schema.UserLogin,  db: Session = Depends(get_db)):
     if get_user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User does not exist")
 
-    stored_password_bytes = get_user.password.encode("utf-8")
-    input_password_bytes = user.password.encode("utf-8")
-
     # 비밀번호 검증
-    if bcrypt.checkpw(input_password_bytes, stored_password_bytes):
-        return encode_jwt(user.phone, days=7)
+    if bcrypt.checkpw(user.password.encode("utf-8"), get_user.password.encode("utf-8")):
+        access_token = encode_jwt(user.phone, minutes=60)
+        token = token_schema.Token(user_id=get_user.id, access_token=access_token, create_at=datetime.now())
+        if read_token(db, get_user.id):
+            update_token(db, token)
+        else:
+            create_token(db, token)
+        return access_token
     else:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
+
+@router.get("/logout")
+async def logout(
+        db: Session = Depends(get_db),
+        access_token: str = Header(None, convert_underscores=False)
+):
+    get_token = read_token_chk(db, access_token)
+    if get_token:
+        return delete_token(db, access_token)
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="No token provided")
+
 
 
